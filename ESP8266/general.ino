42b4f3e09ab94e622ea7d19be2c93f65714aba81
Jeremy Prieto <100392593+JeremyPSS@users.noreply.github.com>
2022-12-12 16:24:57 -0500

    Create general.ino

---
+++ ESP8266/general.ino
@@ -0,0 +1,7 @@
+int sensorValue = analogRead(A0); //leer una entrada de voltaje por ese pin (OJO NO DEBE SER >5V)
+
+mysqli_fetch_array //Obtiene una fila de resultados como un array asociativo, numérico, o ambos
+
+$result = mysqli_query($con,"SELECT * FROM ESPtable2");//obtenemos la tabla en un array
+
+PHP isset(): Comprobar si una variable está definida. Rápido y sencillo post sobre la función PHP isset(), dicha función comprueba si una variable está definida o no en el script de PHP que se está ejecutando.
